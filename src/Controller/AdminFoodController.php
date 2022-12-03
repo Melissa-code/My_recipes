09cae064a8bfd5cd26a62ffaf19947d69b8d9017
Melissa Benard <benardmeli@yahoo.fr>
2022-12-03 16:14:05 +0100

    delete food

--- src/Controller/AdminFoodController.php
+++ src/Controller/AdminFoodController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Food;
 use App\Form\FoodType;
 use App\Repository\FoodRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,6 +15,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminFoodController extends AbstractController
 {
     /**
+     * Get food function
+     *
      * @param FoodRepository $foodRepository
      * @return Response
      */
@@ -29,11 +32,13 @@ class AdminFoodController extends AbstractController
     }
 
     /**
+     * Create or update food function
+     *
      * @param Food $food
      * @return Response
      */
     #[Route('/admin/food/create', name: 'app_create_ingredient')]
-    #[Route('/admin/food/{id}', name: 'app_update_ingredient')]
+    #[Route('/admin/food/{id}', name: 'app_update_ingredient', methods: 'GET|POST')]
     public function createOrUpdateIngredient(Food $food = null, Request $request, ManagerRegistry $managerRegistry)
     {
         if(!$food) {
@@ -44,8 +49,10 @@ class AdminFoodController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $isUpdating = $food->getId() !== null;
             $managerRegistry->getManager()->persist($food);
             $managerRegistry->getManager()->flush();
+            $this->addFlash('success', ($isUpdating) ?'La modification de l\'aliment '.$food->getName().' a bien été effectuée.' : 'L\'ajout de l\'aliment '.$food->getName().' a bien été effectué.' );
             return $this->redirectToRoute('app_admin_food');
         }
 
@@ -54,8 +61,28 @@ class AdminFoodController extends AbstractController
             'form' => $form->createView(),
             'isUpdating' => $food->getId() !== null
         ]);
+    }
 
 
+    /**
+     * Delete food function
+     *
+     * @param Food $food
+     * @param Request $request
+     * @param ManagerRegistry $managerRegistry
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    #[Route('/admin/food/{id}', name: 'app_delete', methods: 'DEL')]
+    public function deleteIngredient(Food $food, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        if($this->isCsrfTokenValid("REM".$food->getId(), $request->get("_token"))) {
+            $entityManager->remove($food);
+            $entityManager->flush();
+            $this->addFlash('success', 'La suppression de l\'aliment '.$food->getName().' a bien été effectuée.');
+            return $this->redirectToRoute('app_admin_food');
+        }
     }
 
 
