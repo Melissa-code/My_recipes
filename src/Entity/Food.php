ec3963c2f23650e79b8f9b157cc8d9bcd46f9bf4
Melissa Benard <benardmeli@yahoo.fr>
2022-12-09 17:27:13 +0100

    data form controls

--- src/Entity/Food.php
+++ src/Entity/Food.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\FoodRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: FoodRepository::class)]
 class Food
@@ -14,25 +15,40 @@ class Food
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\Length(min:3, max:50, minMessage: 'Le nom doit avoir au minimum 3 caractères', maxMessage: 'Le nom doit avoir au maximum 50 caractères')]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Assert\Range(notInRangeMessage: 'Le prix doit être compris entre {{ min }} € et {{ max }} €', min: 0.1, max: 200,)]
+    #[Assert\NotNull]
     private ?float $price = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\Length(min:3, max:100, minMessage: 'L\image doit avoir au minimum 3 caractères', maxMessage: 'L\image doit avoir au maximum 100 caractères')]
+    #[Assert\NotBlank]
     private ?string $image = null;
 
     #[ORM\Column]
+    #[Assert\Range(notInRangeMessage: 'Les calories doivent être comprises entre {{ min }} et {{ max }}', min: 0.1, max: 180,)]
+    #[Assert\NotNull]
     private ?int $calorie = null;
 
     #[ORM\Column]
+    #[Assert\Range(notInRangeMessage:'Les protéines doivent être comprises entre {{ min }} et {{ max }}', min: 0.1, max: 180,)]
+    #[Assert\NotNull]
     private ?float $protein = null;
 
     #[ORM\Column]
+    #[Assert\Range(notInRangeMessage: 'Les glucides doivent être compris entre {{ min }} et {{ max }}', min: 0.1, max: 180,)]
+    #[Assert\NotNull]
     private ?float $carbohydrate = null;
 
     #[ORM\Column]
+    #[Assert\Range(notInRangeMessage: 'Les lipides doivent être compris entre {{ min }} et {{ max }}', min: 0.1, max: 180,)]
+    #[Assert\NotNull]
     private ?float $lipid = null;
+
 
     public function getId(): ?int
     {
